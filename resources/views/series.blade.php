c2b257c2711994ec52bfcfbfcd01c39276c5b84e
Wictor-dev <wictorgabrielmourao@gmail.com>
2021-07-18 21:18:12 -0300

    Exibição de séries feita

--- resources/views/series.blade.php
+++ resources/views/series.blade.php
@@ -16,63 +16,27 @@
 @php
 $count = $series->count();
 @endphp
-@if ($count < 2)
+@if ($count < 6)
 @foreach ($series as $serie)
         <div class="col">
             <div class="card" style="background-color:gray; width:20rem;">
             <img src="/img/got.jpg" alt="">
                 <a href="" style="color: white;"><h2 class="card-title">{{ $serie->name_serie }}</h2></a>
-                <p class="card-text">{{ $serie->sinopse }}</p>
             </div>
         </div>
 @endforeach
 @else
-    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <div class="row">
-                    <div class="col">
-                        <img class="d-block w-100" src="/img/got.jpg" alt="Primeiro Slide">
-                    </div>
-                    <div class="col">
-                        <img class="d-block w-100" src="/img/got.jpg" alt="Primeiro Slide">
-                    </div>
-                    <div class="col">
-                        <img class="d-block w-100" src="/img/got.jpg" alt="Primeiro Slide">
-                    </div>
-                    <div class="col">
-                        <img class="d-block w-100" src="/img/got.jpg" alt="Primeiro Slide">
-                    </div>
-                    <div class="col">
-                        <img class="d-block w-100" src="/img/got.jpg" alt="Primeiro Slide">
-                    </div>
-                </div>
+    @for($i=0; $i < 5; $i++)
+        <div class="col">
+            <div class="card" style="background-color:gray; width:20rem; height: 29rem;">
+            <img src="/img/got.jpg" alt="">
+                <a href="" style="color: white;"><h2 class="card-title">{{ $series[$i]->name_serie }}</h2></a>
             </div>
-            <div class="carousel-item">
-                <div>
-                asdasds
-                </div>
-                <div>
-                asdasds
-                </div>
-                <div>
-                asdasds
-                </div>
-                <div>
-                asdasds
-                </div>
-            </div>
-            
+        @if ($i == 4)
+            <a href="" class="link-primary" style="float:right;">Ver mais...</a>
+        @endif
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Anterior</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Próximo</span>
-        </a>
-    </div>
+    @endfor
 @endif
 </div>
     </div>
